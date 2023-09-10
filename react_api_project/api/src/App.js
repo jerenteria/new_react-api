30d5a4853e6edbbfc8dc9ab602bb82fb371e2712
jerenteria <renteria.juan96@gmail.com>
2023-09-10 12:06:41 -0700

    finished filtering feature

--- react_api_project/api/src/App.js
+++ react_api_project/api/src/App.js
@@ -4,30 +4,32 @@ import UserList from './components/UserList';
 
 function App() {
   const [users, setUsers] = useState([]);
-
-
-  const searchInput = document.querySelector("[dataSearch]")
-
-  searchInput.addEventListener("input", e => {
-    const value = e.target.value
-    console.log(value)
-  })
+  const [query, setQuery] = useState('');
 
   async function fetchData() {
     await fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      return response.json();
-    }).then(data => { 
-      setUsers(data);
-    });
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        setUsers(data);
+      });
   };
 
 
+  const filterNames = query.length === 0 ? users :
+  users.filter(users => users.name.toLowerCase().includes(query.toLowerCase()));
+
   return (
-    <div class="container"> 
+    <div class="container">
       <section class="rendered-data">
-        <input type="search" id="search-bar" placeholder="Search..." dataSearch />
-        <UserList users={users} />
+        <input type="search" 
+        id="search-bar" 
+        placeholder="Search..." 
+        onChange={e => {
+          setQuery(e.target.value) 
+          }} 
+          />
+        <UserList users={filterNames} />
         <button id="button" onClick={fetchData}>Fetch Data!</button>
       </section>
     </div>
